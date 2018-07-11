9156b01caf3674d7a40d899a64310f786e3356f4
Gary Lin <glin@suse.com>
2018-07-11 16:52:12 +0800

    Add lighttpd service

---
+++ lighttpd/lighttpd-start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+conf_dir="/conf"
+if [ ! -d "$conf_dir" ]; then
+	echo "Please ensure '$conf_dir' folder is available."
+	echo 'If you just want to keep your configuration in "conf/", add -v "$(pwd)/conf:/conf" to the docker run command line.'
+	exit 1
+fi
+
+lighttpd_conf="$conf_dir/lighttpd.conf"
+if [ ! -r "$lighttpd_conf" ]; then
+	echo "Please ensure '$lighttpd_conf' exists and is readable."
+	echo "Run the container with arguments 'man lighttpd.conf' if you need help with creating the configuration."
+	exit 1
+fi
+
+cp $lighttpd_conf /etc/lighttpd/lighttpd.conf
+exec /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
